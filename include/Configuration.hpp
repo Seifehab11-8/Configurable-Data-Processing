02882d7b47265801a9db4dce37f8cf4a6af91a0a
Seifehab11-8 <semaziz2004@yahoo.com>
2025-04-16 17:39:46 +0200

    update

--- include/Configuration.hpp
+++ include/Configuration.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include <tuple>
 #include <string>
+#include <map>
 class Configuration {
     public:
     static std::tuple<std::string, double, int> configure(std::string path);
+    static std::map<std::string, std::string> JsonParse(std::string path);
 };
