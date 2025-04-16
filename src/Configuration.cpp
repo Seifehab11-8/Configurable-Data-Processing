f29f5bc4880adff6379bc992ed67f1c33d4aa7c9
Seifehab11-8 <semaziz2004@yahoo.com>
2025-04-16 17:33:37 +0200

    update json parsing is working with numerics and strings

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -1,54 +1,91 @@
 #include "../include/Configuration.hpp"
 #include <fstream>
 #include <iostream>
+#include "../include/Configuration.hpp"
 
 std::tuple<std::string, double, int> Configuration::configure(std::string path)
 {
     std::tuple<std::string, double, int> config;
+    try {
+        std::map<std::string, std::string> jsonKeyMap = JsonParse(path);
+        std::get<0>(config) = jsonKeyMap["Type"];
+        std::get<1>(config) = std::stod(jsonKeyMap["Threshold"]);
+        std::get<2>(config) = std::stoi(jsonKeyMap["MaxRetries"]);
+    }
+    catch (const std::exception &e){
+        std::cerr<<e.what()<<std::endl;
+    }
+    return config;
+}
+std::map<std::string, std::string> Configuration::JsonParse(std::string path)
+{
+    std::map<std::string, std::string> jsonKeyMap;
     std::ifstream ifs(path);
 
+    bool key_found = false, 
+    value_found = false, 
+    str_found = false, 
+    key_created = false, 
+    value_created = false;
+
+    std::string key_builder, value_builder;
     if(!ifs.is_open()) {
         std::cerr<<"Failed to open the file"<<std::endl;
-        return config;
+        throw std::runtime_error("Couldn't find the file");
     }
 
-    bool value_found = false, value_obtained = false;
-    int counter = 0;
-
     try{
-        std::string line;
-        std::string value;
-        while(getline(ifs, line)) {
-            value_obtained = false;
-            value_found = false;
-            value.clear();
-            for(auto ch: line) {
-                if(ch == ',') {
-                    value_obtained = true;
-                    value_found = false;
-                }
-                if(ch == '{') {
-                    value_obtained = false;
-                    value_found = false;
-                    break;
-                }
-                if(value_found && ch != ' ' && ch != '"') {
-                    value+=ch;
-                }
-                if(ch == ':') {
-                    value_found = true;
+        char ch;
+        while(ifs.get(ch)) {
+            if(key_found) {
+                while(ifs.get(ch)) {
+                    if(ch == '"'&& str_found) {
+                        key_found = false;
+                        str_found = false;
+                        key_created = true;
+                        break;
+                    }
+                    else if(ch == '"' && !str_found) {
+                        str_found = true;
+                        continue;
+                    }
+                    if(str_found) key_builder+= ch;
                 }
             }
             
-            if(value_obtained) {
-                if (counter == 0) {
-                    std::get<0>(config) = value;
-                } else if (counter == 1) {
-                    std::get<1>(config) = std::stod(value);
-                } else if (counter == 2) {
-                    std::get<2>(config) = std::stoi(value);
+            if(value_found && key_created) {
+                while(ifs.get(ch)) {
+                    if(ch == '"' && !str_found) {
+                        value_builder.clear();
+                        str_found = true;
+                        continue;
+                    }
+                    else if(ch == '"' && str_found) {
+                        value_found = false;
+                        value_created = true;
+                        str_found = false;
+                        key_found = true;
+                        break;
+                    }
+                    else if(ch == ',' || ch == '}') {
+                        value_found = false;
+                        value_created = true;
+                        value_builder.erase(std::remove(value_builder.begin(),
+                                            value_builder.end(), ' '),value_builder.end());
+                        key_found =true;
+                        break;
+                    }
+                    value_builder+=ch;
                 }
-                counter++;
+            }
+            if(ch == '{') key_found = true;
+            if(ch == ':') value_found = true;
+            if(value_created && key_created) {
+                jsonKeyMap[key_builder] = value_builder;
+                value_created = false;
+                key_created = false;
+                value_builder.clear();
+                key_builder.clear();
             }
         }
     }
@@ -56,5 +93,5 @@ std::tuple<std::string, double, int> Configuration::configure(std::string path)
         std::cerr<<"Error Occurred. . ."<<std::endl;
     }
     ifs.close();
-    return config;
+    return jsonKeyMap;
 }
